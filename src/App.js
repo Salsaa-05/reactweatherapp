d3d80249d5e43ef87c7b2e330c7ec3fe6795f156
Administrator <saadhamouda95@gmail.com>
2020-05-14 23:53:45 +0200

    Add flags

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-undef */
 import React, { Component } from 'react';
 import Weather from './Component/Weather'
 import Form from './Component/Form'
 import Sun from './Component/Sun'
+import Change from './Component/Changebutton'
+
 const API_KEY = "e36ed364400282e43250b6c4c0274d44";
 
 class App extends Component {
@@ -24,8 +27,12 @@ class App extends Component {
     const data = await api.json();
 
     if (city && country) {
+
+      console.log(city, country)
       this.setState({
+        // tempreature: this.Changetemp(data.main.temp),
         tempreature: data.main.temp,
+
         city: data.name,
         country: data.sys.country,
         humidity: data.main.humidity,
@@ -52,13 +59,32 @@ class App extends Component {
   ////////prova
 
 
+  Changetemp = (fahren) => {
+    console.log(fahren)
+    const celsius = (fahren - 32) * 5 / 9
+    if (typeof celsius !== "undefined") {
+      return celsius;
+    } else {
+      return "celsius not defined";
+
+    }
+
+  }
+
+
+
   render() {
     return (
       <div>
         <Sun />
         <div className="App" >
           <div className="form-countainer">
-            <Form getWeather={this.getWeather} getWeathertorino={this.getWeathertorino} />
+            <Form getWeather={this.getWeather} />
+            <Change Changetemp={this.Changetemp}
+            />
+            <img src={'/torino.' + this.state.country + '.png'} alt='torino' width='100%' />
+            <img src={'/Lisbon.' + this.state.country + '.png'} alt='portgal' width="100%" />
+            <img src={'/London.' + this.state.country + '.png'} alt='london'width='100%'/>
             <Weather
               tempreature={this.state.tempreature}
               city={this.state.city}
---
+++ src/Component/Changebutton.js
@@ -0,0 +1,15 @@
+import React  from 'react'
+
+const Change =  (props) => {
+
+return(
+<div>
+<button onSubmit={props.Changetemp}>Change Temp</button>
+
+</div>
+
+
+)
+
+}
+ export default Change;
